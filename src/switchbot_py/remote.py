77745d36551a999c0748051f72684baa0270b73f
gsy0911 <yoshiki0911@gmail.com>
2025-01-31 09:26:23 +0900

    add remote command

--- src/switchbot_py/remote.py
+++ src/switchbot_py/remote.py
@@ -13,6 +13,7 @@ class InfraredRemoteDto(BaseModel):
 class InfraredRemote:
     _remote_type: ClassVar[str]
     _remote_type_mapping: ClassVar[dict[str, Type["InfraredRemote"]]] = {}
+    _funcs_to_attach: ClassVar[list[str]] = []
 
     def __init__(
         self,
@@ -27,6 +28,9 @@ class InfraredRemote:
         self.device_id = device_id
         self.device_name = device_name
         self.hub_device_id = hub_device_id
+        # attach required functions
+        for func in self._funcs_to_attach:
+            setattr(self, func[1:], getattr(self, func))
 
     def __init_subclass__(cls):
         if cls._remote_type is not None:
@@ -53,14 +57,52 @@ class InfraredRemote:
     def __repr__(self) -> str:
         return f"Remote(type={self._remote_type}, name={self.device_name}, device_id={self.device_id})"
 
+    def _command(
+        self,
+        command: str,
+        command_type: str | None = None,
+        parameter: str | None = None,
+    ) -> dict:
+        url = f"https://api.switch-bot.com/v1.1/devices/{self.device_id}/commands"
+        data = {"command": command}
+        if command_type:
+            data.update({"commandType": command_type})
+        if parameter:
+            data.update({"parameter": parameter})
+        res = self._client.post(url, data=data)
+        return res
+
+    def _turn_on(self):
+        self._command(command="turnOn")
+
+    def _turn_off(self):
+        self._command(command="turnOff")
+
 
 class RemoteAirConditioner(InfraredRemote):
     _remote_type = "Air Conditioner"
+    _funcs_to_attach = ["_turn_off"]
+
+    def _turn_on_air_conditioner(self, temperature: int, mode: int, fan: int):
+        res = self._command(
+            command="setAll",
+            command_type="command",
+            parameter=f"{temperature},{mode},{fan},on",
+        )
+        return res
+
+    def turn_on_with_cool(self, temperature: int):
+        return self._turn_on_air_conditioner(temperature=temperature, mode=2, fan=1)
+
+    def turn_on_with_hot(self, temperature: int):
+        return self._turn_on_air_conditioner(temperature=temperature, mode=5, fan=1)
 
 
 class RemoteLight(InfraredRemote):
     _remote_type = "Light"
+    _funcs_to_attach = ["_turn_on", "_turn_off"]
 
 
 class RemoteDiyTv(InfraredRemote):
     _remote_type = "DIY TV"
+    _funcs_to_attach = ["_turn_on", "_turn_off"]
